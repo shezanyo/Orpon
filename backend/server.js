a3b958c9192fa06f07949f184ad00e00df01151c
shezanyo <coolalimiad@gmail.com>
2026-05-29 18:00:31 +0600

    feat: prepare for Azure deployment

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use("/api", authRoutes);
 app.use("/api", campaignRoutes);
 app.use("/api", donationRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
